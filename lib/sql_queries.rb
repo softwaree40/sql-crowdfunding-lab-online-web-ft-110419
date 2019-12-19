ef5dda53fb960069316fbbba39882d12cf403bb2
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-19 06:05:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,11 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "Write your SQL query Here"
-  "SELECT users.name SUM(pledges) GROUP BY name ORDER BY SUM(pledges)"
+  "SELECT users.name, SUM(pledges.amount)
+FROM users JOIN pledges
+ON users.id = pledges.user_id
+GROUP BY users.name
+ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
